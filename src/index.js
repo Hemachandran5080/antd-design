0288801cb21b29921733478bfa45f869f48782b0
Hemachandran <hemavikic@gmail.com>
2021-08-15 14:59:16 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Body from './components/Body';
+import SignInSide from './components/SignInSide';
+import { BrowserRouter as Router, Route,Switch,withRouter} from "react-router-dom";
+import Navbar from './components/Navbar';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={SignInSide} />
+      <Route path="/employeestable" component={Navbar} />
+      <Route path="/chart" component={Body} />
+      <Route />
+      <Route />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
@@ -15,3 +25,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
